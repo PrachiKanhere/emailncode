a1c44b79438308a423ef4804c412fca59d117491
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-12 11:15:32 +0530

    Create down_bus_check.py

---
+++ .github/workflows/down_bus_check.py
@@ -0,0 +1,18 @@
+import re
+
+
+text_file = open("abc1.txt", "r")
+str = text_file.read()
+
+
+match = re.search(r"Following Buses might be down: \[.*\]", str)
+if match:
+       downBus= match.group()
+       print(downBus)
+       DOWN_BUS = downBus.split(": ")[1]
+       print(DOWN_BUS)
+       if len(DOWN_BUS)== 0:
+           print('Down Bus not defined')
+else:
+    print('Message Engines up and running')
+text_file.close()
