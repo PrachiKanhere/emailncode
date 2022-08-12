a8f8ae061ac1c77dac01b6db3cde49a64e0773f2
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-12 15:27:36 +0530

    Update down_bus_check.py

--- .github/workflows/down_bus_check.py
+++ .github/workflows/down_bus_check.py
@@ -7,6 +7,7 @@ print(str)
 
 match = re.search(r"Following Buses might be down: \[.*\]", str)
 if match:
+       print('hitting')
        downBus= match.group()
        #print(downBus)
        DOWN_BUS = downBus.split(": ")[1]
@@ -14,6 +15,6 @@ if match:
        if len(DOWN_BUS)== 0:
            print('Down Bus not defined')
 else:
-    print('Message Engines up and running')
+       print('Message Engines up and running')
 
 text_file.close()
