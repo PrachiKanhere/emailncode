50bd2921d8ba23ee2692c1f3273fa5ff3eb963da
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-18 19:26:56 +0530

    Create fail2.py

---
+++ .github/workflows/fail2.py
@@ -0,0 +1,11 @@
+import re
+
+
+text_file = open("abc1.txt", "r")
+str = text_file.read()
+print(str)
+
+match = re.search(r"has exceeded threshold limit for messages. Please take action!", str)
+if match:
+       print('hitting')
+       print(True)
