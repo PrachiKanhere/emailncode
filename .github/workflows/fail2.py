3b1cda947a8816e06c04bb84031a252840dfc679
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-19 15:25:37 +0530

    Update fail2.py

--- .github/workflows/fail2.py
+++ .github/workflows/fail2.py
@@ -3,7 +3,7 @@ import re
 
 text_file = open("abc1.txt", "r")
 print(text_file.read())
-pattern = re.compile('has exceeded threshold limit for messages. Please take action!')
+pattern = re.compile(r'has exceeded threshold limit for messages. Please take action!')
 
 match = pattern.search(text_file.read())
 if match:
