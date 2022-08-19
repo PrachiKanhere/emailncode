a204e66afc3d38d78d3f7eebfaedb1c50d2d5b90
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-19 14:40:56 +0530

    Update fail2.py

--- .github/workflows/fail2.py
+++ .github/workflows/fail2.py
@@ -5,7 +5,7 @@ text_file = open("abc1.txt", "r+")
 str = text_file.read()
 #print(str)
 
-match = re.search(r"^has exceeded threshold limit for messages. Please take action!$", str)
+match = re.findall(r"^has exceeded threshold limit for messages. Please take action!$", str, re.MULTILINE)
 if match:
     print("hitting case")
     print('match')
