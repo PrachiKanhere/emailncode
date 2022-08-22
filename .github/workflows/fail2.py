4c778fd1d6a33f15f0a12ae98513d5a4c46179cc
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-22 16:46:16 +0530

    Update fail2.py

--- .github/workflows/fail2.py
+++ .github/workflows/fail2.py
@@ -4,8 +4,22 @@ import re
 text_file = open("abc1.txt", "r")
 str = text_file.read()
 
-if re.search(r"has exceeded threshold limit for messages. Please take action!",str):
-       print('Match')
+str1 = "has exceeded threshold limit for messages. Please take action!"
+str2 = "There might be problem with WAS servers."
+str3 = "Issue Has Occurred for Allowed Retrial Times"
+str4 = "Following Queues are not reducing."
+str5 = "Following Buses might be down."
 
-       
+
+if re.search(r'str1,str):
+             print(str1)
+elif re.search(r'str2,str):
+               print(str2)
+elif re.search(r'str3,str):
+               print(str3)
+elif re.search(r'str4,str):
+               print(str4)
+elif re.search(r'str5,str):
+               print(str5)
+          
 text_file.close()
